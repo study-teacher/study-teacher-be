e040d2457b8b5e5159e450a92d2b1ef9c0407a54
silvarge <sep.non2@gmail.com>
2025-11-09 05:01:55 +0900

    init: Project initialization

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # study-teacher-be
+
+[![codecov](https://codecov.io/github/study-teacher/study-teacher-be/graph/badge.svg?token=RPNNN5EOXL)](https://codecov.io/github/study-teacher/study-teacher-be)
+
 공부하세요 선생님! 의 백엔드 레포지토리
---
+++ src/main/java/com/fridge/studyteacherbe/StudyTeacherBeApplication.java
@@ -0,0 +1,13 @@
+package com.fridge.studyteacherbe;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class StudyTeacherBeApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(StudyTeacherBeApplication.class, args);
+  }
+
+}
