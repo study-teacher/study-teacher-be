dcd8ddd385f4ab0069db3673fb280a3f96721347
silvarge <sep.non2@gmail.com>
2025-11-10 02:19:33 +0900

    init: HealthCheckController 추가
    일단 actuator를 통해 헬스 체크를 진행 하지만 유사시 대비를 위함

--- src/main/java/com/fridge/studyteacherbe/global/health/HealthCheckController.java
+++ src/main/java/com/fridge/studyteacherbe/global/health/HealthCheckController.java
@@ -2,9 +2,11 @@ package com.fridge.studyteacherbe.global.health;
 
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-@RestController("/health")
+@RestController
+@RequestMapping("/health")
 public class HealthCheckController {
 
   @GetMapping
